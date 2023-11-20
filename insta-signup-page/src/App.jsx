7f4c066c81e4854767c6be8410dc35aa64317c9d
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2023-11-20 15:43:06 +0530

    insta signup page

--- insta-signup-page/src/App.jsx
+++ insta-signup-page/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Importing necessary dependencies from React
+import { useState } from 'react';
+import './App.css';
 
+// Importing the Instagram logo image
+import logo from "./assets/instagram.png"
+
+// Functional component App
 function App() {
-  const [count, setCount] = useState(0)
+  // Using the useState hook to manage the login state
+  const [login, setLogin] = useState(true);
 
+  // Function to toggle between login and signup
+  const loginSwitch = () => {
+    setLogin(!login);
+  }
+
+  // JSX structure for the component
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id='Container'>
+      <div id="user">
+        {/* Instagram logo */}
+        <img src={logo} alt="" />
+
+        {/* Conditional rendering for input fields based on the login state */}
+        <input hidden={login} type="text" name="" placeholder='Mobile Number or Email' id="" />
+        <input hidden={login} type="text" name="" placeholder='Full Name' id="" />
+
+        {/* Common input fields for both login and signup */}
+        <input type="email" name="" id="" placeholder='Phone number, username, or email'/>
+        <input type="text" name="" id="" placeholder='Password' />
+
+        {/* Button with dynamic text based on login state */}
+        <button>{login ? "Sign in" : "Sign up"}</button>
+
+        {/* Footer section with conditional text and a clickable span to switch between login and signup */}
+        <div id='footer'>
+          {login ? "Don't have an account?" : "Have an account?"} <span onClick={loginSwitch}>{login ? "Sign up" : "Log in"}</span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+// Exporting the App component as the default export
+export default App;
